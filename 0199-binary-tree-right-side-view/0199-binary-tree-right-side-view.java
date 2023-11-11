c8e497a3127a429d8261de3ef7d21f5e9d1c77f9
anurag444 <37261718+anurag444@users.noreply.github.com>
2023-11-11 10:18:34 -0500

    Time: 0 ms (100%), Space: 41 MB (73.69%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -14,28 +14,57 @@
  * }
  */
 class Solution {
+    // RECURSIVE APPROACH
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> res = new ArrayList<>();
 
         //base case
         if(root == null) return res;
-
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-
-        while(!q.isEmpty()){
-            int size = q.size();
-
-            for(int i = 0; i < size; i++){
-                TreeNode node = q.remove();
-                if(i == size - 1){
-                    res.add(node.val);
-                }
-                if(node.left != null) q.add(node.left);
-                if(node.right != null) q.add(node.right);
-            }
-        }
-
+        rightViewHelper(root, res, 0);
         return res;
     }
+
+    public void rightViewHelper(TreeNode node, List<Integer> res, int depth){
+        if(node == null){
+            return;
+        }
+
+        if(depth == res.size()){
+            res.add(node.val);
+        }
+
+        rightViewHelper(node.right, res, depth + 1);
+        rightViewHelper(node.left, res, depth + 1);
+
+    }
+
+
+
+
+
+    // ITERATIVE APPROCH
+    // public List<Integer> rightSideView(TreeNode root) {
+    //     List<Integer> res = new ArrayList<>();
+
+    //     //base case
+    //     if(root == null) return res;
+
+    //     Queue<TreeNode> q = new LinkedList<>();
+    //     q.add(root);
+
+    //     while(!q.isEmpty()){
+    //         int size = q.size();
+
+    //         for(int i = 0; i < size; i++){
+    //             TreeNode node = q.remove();
+    //             if(i == size - 1){
+    //                 res.add(node.val);
+    //             }
+    //             if(node.left != null) q.add(node.left);
+    //             if(node.right != null) q.add(node.right);
+    //         }
+    //     }
+
+    //     return res;
+    // }
 }
