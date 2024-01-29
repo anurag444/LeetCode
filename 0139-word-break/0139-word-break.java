2754f60aa20c4bd079038fb141a095f618822494
anurag444 <37261718+anurag444@users.noreply.github.com>
2024-01-29 11:51:39 -0500

    Time: 8 ms (43.31%), Space: 44.2 MB (38.44%) - LeetHub

---
+++ 0139-word-break/0139-word-break.java
@@ -0,0 +1,24 @@
+class Solution {
+    
+    //HashSet Solution
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>(wordDict);
+        int n = s.length();
+        
+        boolean[] dp = new boolean[n + 1];
+        
+        //Set the starting point as true
+        dp[0] = true;
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = 0; j < i; j++){
+                //check if substring exists in the set and starting point is true means we have successfull word break till that point
+                if(dp[j] && set.contains(s.substring(j, i))){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+}
