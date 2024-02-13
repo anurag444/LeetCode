e360a13414d02916377cece1b5da4d7f41489ced
anurag444 <37261718+anurag444@users.noreply.github.com>
2024-02-13 11:45:53 -0500

    Time: 1 ms (78.43%), Space: 45.4 MB (62.69%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,26 +1,56 @@
 class Solution {
+    
     public void setZeroes(int[][] matrix) {
-        int m = matrix.length;
         int n = matrix[0].length;
-        Set<Integer> rows = new HashSet<>();
-        Set<Integer> cols = new HashSet<>();
+        int m = matrix.length;
         
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
+        boolean isRowZero = false;
+        boolean isColZero = false;
+        
+        for(int i = 1; i< n; i++){
+            if(matrix[0][i] == 0){
+                isRowZero = true;
+            }
+        }
+        
+        for(int i = 1; i< m; i++){
+            if(matrix[i][0] == 0){
+                isColZero = true;
+            }
+        }
+        
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
                 if(matrix[i][j] == 0){
-                    rows.add(i);
-                    cols.add(j);
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
         
-        for(int row: rows){
-            setZeroRow(row, matrix);
+        
+        
+        for(int i = 1; i< n; i++){
+            if(matrix[0][i] == 0){
+                setZeroCol(i, matrix);
+            }
+        }
+        for(int i = 1; i< m; i++){
+            if(matrix[i][0] == 0){
+                setZeroRow(i, matrix);
+            }
         }
         
-        for(int col: cols){
-            setZeroCol(col, matrix);
+        
+        
+        if(matrix[0][0] == 0){
+            setZeroRow(0, matrix);
+            setZeroCol(0, matrix);
         }
+        
+        if(isColZero) setZeroCol(0, matrix);
+        if(isRowZero) setZeroRow(0, matrix);
+            
     }
     
     public void setZeroRow(int row, int[][] matrix){
@@ -36,4 +66,32 @@ class Solution {
             matrix[row][col] = 0;
         }
     }
+    
+    
+    // O(n + m) space solution
+//     public void setZeroes(int[][] matrix) {
+//         int m = matrix.length;
+//         int n = matrix[0].length;
+//         Set<Integer> rows = new HashSet<>();
+//         Set<Integer> cols = new HashSet<>();
+        
+//         for(int i = 0; i < m; i++){
+//             for(int j = 0; j < n; j++){
+//                 if(matrix[i][j] == 0){
+//                     rows.add(i);
+//                     cols.add(j);
+//                 }
+//             }
+//         }
+        
+//         for(int row: rows){
+//             setZeroRow(row, matrix);
+//         }
+        
+//         for(int col: cols){
+//             setZeroCol(col, matrix);
+//         }
+//     }
+    
+
 }
