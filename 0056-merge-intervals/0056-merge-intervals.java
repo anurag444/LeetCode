c57201071dfcd67fbb3ba6b40f931b174e4bbdba
anurag444 <37261718+anurag444@users.noreply.github.com>
2024-02-25 15:57:18 -0500

    Time: 10 ms (35.82%), Space: 45.9 MB (97.62%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,54 +1,71 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, Comparator.comparingInt(interval -> interval[0]));
-        List<int[]> res = new ArrayList<>();
+        //clean solution
         
-        int size = intervals.length;
-        if(size == 1) return intervals;
+        List<int[]> ans = new ArrayList<>();
         
-        //first, add to the list
-        int f1 = intervals[0][0];
-        int f2 = intervals[0][1];
-            
-        int s1 = intervals[1][0];
-        int s2 = intervals[1][1];
-        
-        int index = 1;
-        
-        if(f2 < s1){
-            res.add(new int[]{f1, f2});
-        }
-        //merging
-        else if(f2 >= s1){
-            res.add(new int[]{Math.min(f1, s1), Math.max(f2, s2)});
-            index = 2;
-        }
-        
-        
-        for(int i = index; i < size; i++){
-            int x1 = res.get(res.size() - 1)[0];
-            int x2 = res.get(res.size() - 1)[1];
-            
-            int y1 = intervals[i][0];
-            int y2 = intervals[i][1];
-            
-            //case 1 - no merging
-            if(x2 < y1){
-                res.add(new int[]{y1, y2});
-            }
-            //merging
-            else if(x2 >= y1){
-                //remove from the res
-                res.remove(res.size() - 1);
-                res.add(new int[]{Math.min(x1, y1), Math.max(x2, y2)});
+        for (int[] interval : intervals) {
+            if (ans.isEmpty() || interval[0] > ans.get(ans.size() - 1)[1]) {
+                ans.add(interval);
+            } else {
+                ans.get(ans.size() - 1)[1] = Math.max(interval[1], ans.get(ans.size() - 1)[1]);
             }
         }
         
-        int n = res.size();
-        int[][] out = new int[n][2];
+        return ans.toArray(new int[ans.size()][]);
         
-        res.toArray(out);
         
-        return out;
+        
+        
+//         List<int[]> res = new ArrayList<>();
+        
+//         int size = intervals.length;
+//         if(size == 1) return intervals;
+        
+//         //first, add to the list
+//         int f1 = intervals[0][0];
+//         int f2 = intervals[0][1];
+            
+//         int s1 = intervals[1][0];
+//         int s2 = intervals[1][1];
+        
+//         int index = 1;
+        
+//         if(f2 < s1){
+//             res.add(new int[]{f1, f2});
+//         }
+//         //merging
+//         else if(f2 >= s1){
+//             res.add(new int[]{Math.min(f1, s1), Math.max(f2, s2)});
+//             index = 2;
+//         }
+        
+        
+//         for(int i = index; i < size; i++){
+//             int x1 = res.get(res.size() - 1)[0];
+//             int x2 = res.get(res.size() - 1)[1];
+            
+//             int y1 = intervals[i][0];
+//             int y2 = intervals[i][1];
+            
+//             //case 1 - no merging
+//             if(x2 < y1){
+//                 res.add(new int[]{y1, y2});
+//             }
+//             //merging
+//             else if(x2 >= y1){
+//                 //remove from the res
+//                 res.remove(res.size() - 1);
+//                 res.add(new int[]{Math.min(x1, y1), Math.max(x2, y2)});
+//             }
+//         }
+        
+//         int n = res.size();
+//         int[][] out = new int[n][2];
+        
+//         res.toArray(out);
+        
+//         return out;
     }
 }
