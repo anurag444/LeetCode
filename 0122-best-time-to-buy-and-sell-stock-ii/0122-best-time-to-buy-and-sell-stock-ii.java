9fe2d07eae86b2424d35a24311af53d601c79c4d
anurag444 <37261718+anurag444@users.noreply.github.com>
2024-03-11 14:40:51 -0400

    Time: 1 ms (93.71%), Space: 46.1 MB (17.60%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -2,30 +2,56 @@ class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
         
-        int[][] dp = new int[n][2];
+        return maxProfitHelperTab(prices, n);
         
-        for (int[] row: dp)
-            Arrays.fill(row, -1);
         
-        return maxProfitHelper(0, 1, prices, n, dp);
+        //return maxProfitHelperMem(0, 1, prices, n, dp);
     }
     
-    private int maxProfitHelper(int index, int buy, int[] prices, int n, int[][] dp){
+    
+    //Memoozimation approach 
+    private int maxProfitHelperMem(int index, int buy, int[] prices, int n, int[][] dp){
         if(index == n) return 0;
         if(dp[index][buy] != -1) return dp[index][buy];
         int profit = 0;
         
         if(buy == 1){
-            profit = Math.max(-prices[index] + maxProfitHelper(index + 1, 0, prices, n, dp),
-                             0 + maxProfitHelper(index + 1, 1, prices, n, dp));
+            profit = Math.max(-prices[index] + maxProfitHelperMem(index + 1, 0, prices, n, dp),
+                             0 + maxProfitHelperMem(index + 1, 1, prices, n, dp));
         }
         else{
-            profit = Math.max(prices[index] + maxProfitHelper(index + 1, 1, prices, n, dp),
-                             0 + maxProfitHelper(index + 1, 0, prices, n, dp));
+            profit = Math.max(prices[index] + maxProfitHelperMem(index + 1, 1, prices, n, dp),
+                             0 + maxProfitHelperMem(index + 1, 0, prices, n, dp));
         }
         
         dp[index][buy] = profit;
         
         return profit;
+    }
+    
+    //Tabulation approach 
+    private int maxProfitHelperTab(int[] prices, int n){
+        int[][] dp = new int[n + 1][2];
+        
+        
+        for(int index = n - 1; index >= 0; index--){
+            int profit = 0;
+            for(int buy = 0; buy <=1; buy++){
+                if(buy == 1){
+                    profit = Math.max(-prices[index] + dp[index + 1][0],
+                             0 + dp[index + 1][1]);
+                }
+                else{
+                    profit = Math.max(prices[index] + dp[index + 1][1],
+                             0 + dp[index + 1][0]);
+                }
+                
+                dp[index][buy] = profit;
+            }
+            
+        }
+        
+        
+        return dp[0][1];
     }
 }
