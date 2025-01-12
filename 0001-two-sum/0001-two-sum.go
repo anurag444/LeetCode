409a7643cf513b18875ed9f10017830404dfe878
Anurag Teckchandani <anuragteckchandani@gmail.com>
2025-01-12 12:13:22 -0500

    Time: 3 ms (46.48%), Space: 6 MB (32.76%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -1,12 +1,26 @@
 func twoSum(nums []int, target int) []int {
-    numMap := make(map[int]int) // Step 1: Create a map to store numbers and their indices.
+    var m map[int]int
+
+    // create a slice of array with 2 elements
+    res := make([]int, 2)
+
+    m = make(map[int]int)
+
     for i, num := range nums {
-        // Step 2: Calculate the complement for each number during iteration.
-        if j, ok := numMap[target-num]; ok {
-            return []int{j, i} // Step 3: If the complement exists, return the indices.
+        x, ok := m[target - num];
+
+        if ok {
+            res[0] = i;
+            res[1] = x;
+
+            break;
         }
-        numMap[num] = i // Step 4: Add the current number and its index to the map.
+
+        m[num] = i;
+
+
     }
-    // Step 5: If no pair sums up to the target, return nil.
-    return nil
+
+    return res;
+    
 }
