d58ca9b32f383a89416e116b1f78462feebbdb73
Anurag Teckchandani <anuragteckchandani@gmail.com>
2025-02-02 09:34:51 -0500

    Time: 17 ms (26.8%), Space: 48.3 MB (29.73%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,54 +1,36 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        
-        int n = strs.length;
-
         List<List<String>> res = new ArrayList<>();
+        HashMap<String, List<String>> map = new HashMap<>();
 
-        for(int i = 0; i < n - 1; i++){
-            String s1 = strs[i];
-            if(s1 == "NA") continue;
-            List<String> temp = new ArrayList<>();
-            temp.add(s1);
-            for(int j = i + 1; j < n; j++){
-                
-                String s2 = strs[j];
-
-                if(s2 != "NA" && checkAnagram(s1, s2)){
-                    temp.add(s2);
-                    strs[j] = "NA";
-                } 
-            }
-
-            res.add(temp);
+        for(String str : strs){
+            createHash(map, str);
         }
 
-        if(strs[n - 1] != "NA"){
-            res.add(new ArrayList<>(Arrays.asList(strs[n - 1])));
+        for(Map.Entry<String,List<String>> entry : map.entrySet()){
+            res.add(entry.getValue());
         }
 
         return res;
-
-
     }
 
-    public boolean checkAnagram(String s1, String s2){
-        if(s1.length() != s2.length()) return false;
+    public void createHash(HashMap<String, List<String>> map, String str){
+        int[] freq = new int[26];
 
-        int[] words = new int[26];
-
-        for(char c : s1.toCharArray()){
-            words[c - 'a']++;
+        for(char c : str.toCharArray()){
+            freq[c - 'a']++;
         }
+        StringBuilder keyBuilder = new StringBuilder();
+            for (int count : freq) {
+                keyBuilder.append("#").append(count);
+            }
+            String hashValue = keyBuilder.toString();
 
-        for(char c : s2.toCharArray()){
-            words[c - 'a']--;
+        if(!map.containsKey(hashValue)){
+            map.put(hashValue, new ArrayList<>());
         }
-
-        for(int i = 0; i < 26; i++){
-            if(words[i] != 0) return false;
-        }
-
-        return true;
+        List<String> list = map.get(hashValue);
+        list.add(str);
+        map.put(hashValue, list);
     }
 }
