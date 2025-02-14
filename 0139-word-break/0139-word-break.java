437c81a097445b33460cf1112df4e27cb4d92e23
Anurag Teckchandani <anuragteckchandani@gmail.com>
2025-02-13 20:30:28 -0500

    Time: 1 ms (99.27%), Space: 41.8 MB (93.37%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,27 +1,28 @@
 class Solution {
-    
-    //HashSet Solution
     public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> set = new HashSet<>(wordDict);
+        //store the dict in set
+
+        Set<String> dict = new HashSet<>(wordDict);
+        int maxLen = 0;
+        //find max len of dict word
+        for(String word : dict){
+            maxLen = Math.max(maxLen, word.length());
+        }
+
         int n = s.length();
-        
         boolean[] dp = new boolean[n + 1];
-        
-        //Set the starting point as true
+
         dp[0] = true;
-        
+
         for(int i = 1; i <= n; i++){
-            for(int j = 0; j < i; j++){
-                //check if substring exists in the set and starting point is true means we have successfull word break till that point
-                if(dp[j] && set.contains(s.substring(j, i))){
+            for(int j = i -1; j >= Math.max(0, i - maxLen); j--){
+                if(dp[j] && dict.contains(s.substring(j, i))){
                     dp[i] = true;
                     break;
                 }
             }
         }
+
         return dp[n];
     }
-    
-    
-    //We can do same approach by starting from end and repeatin gt he same process
 }
