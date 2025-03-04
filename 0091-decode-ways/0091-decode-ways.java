2289029e255a68bd45a2cf37fd38500abb642d20
Anurag Teckchandani <anuragteckchandani@gmail.com>
2025-03-03 19:06:43 -0500

    Time: 0 ms (100%), Space: 41.8 MB (76.02%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,35 +1,42 @@
 class Solution {
     public int numDecodings(String s) {
         int n = s.length();
+        int[] dp = new int[n + 1];
 
-        int[] ways = new int[n + 1];
+        dp[0] = 1;
 
-        ways[0] = 1;
+        for(int i = 1; i <= n; i++){
+            char c = s.charAt(i - 1);
 
-        if(s.charAt(0) == '0') return 0;
+            
+                dp[i] = dp[i - 1];
 
-        ways[1] = 1;
+                if(c == '0'){
 
-        for(int i = 1; i < n; i++){
-            int num = Character.getNumericValue(s.charAt(i));
+                    dp[i] = 0;
+                }
 
-            //check if single digit is valid
-            if(num > 0 && num <= 26){
-                ways[i + 1] = ways[i];
-            }
-
-            //check if double digit is valid
-            int num1 = Character.getNumericValue(s.charAt(i - 1));
-
-            if(num1 == 0) continue;
-
-            num1 = num1 * 10 + num;
-
-            if(num1 > 0 && num1 <= 26){
-                ways[i + 1] += ways[i - 1];
-            }
+                if(i > 1 && (s.charAt(i - 2) == '1' || s.charAt(i - 2) == '2' && s.charAt(i - 1) < '7')){
+                    dp[i] += dp[i - 2];
+                }
+            
         }
 
-        return ways[n];
+        return dp[n];
     }
+
+    // public int ways(int i, String s){
+    //     int n = s.length();
+
+    //     if(i == n) return 1;
+    //     if(s.charAt(i) == '0') return 0;
+
+    //     int res = ways(i +1, s);
+
+    //     if(i < n - 1 &&(s.charAt(i) == '1' || s.charAt(i) == '2' && s.charAt(i + 1) < '7')){
+    //         res += ways(i + 2, s);
+    //     }
+
+    //     return res;
+    // }
 }
