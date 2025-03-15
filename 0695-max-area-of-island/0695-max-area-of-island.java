783208f14631d85e4ffe610e3ad4d37a526f75ba
Anurag Teckchandani <anuragteckchandani@gmail.com>
2025-03-15 10:20:25 -0400

    Time: 1 ms (100%), Space: 44.7 MB (58.74%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,36 +1,29 @@
 class Solution {
-    private int count = 0;
-    private int n;
-    private int m;
     public int maxAreaOfIsland(int[][] grid) {
-        n = grid[0].length;
-        m = grid.length;
-        
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int area = 0;
         int max = 0;
-        
-        
+
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(grid[i][j] == 1){
-                    DFS(i, j, grid);
-                    
-                    max = Math.max(count, max);
-                    count = 0;
+                    area = dfs(grid, i, j);
+
+                    max = Math.max(area, max);
                 }
             }
         }
-        
-        
+
         return max;
     }
-    
-    public void DFS(int i, int j, int[][] grid){
-        if(i < 0 || j < 0 || i >= m || j >= n || grid[i][j] != 1) return;
+
+    public int dfs(int[][] grid, int i, int j){
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0) return 0;
+
         grid[i][j] = 0;
-        count++;
-        DFS(i + 1, j, grid);
-        DFS(i - 1, j, grid);
-        DFS(i, j + 1, grid);
-        DFS(i, j - 1, grid);
+
+        return 1 + dfs(grid, i + 1, j) + dfs(grid, i - 1, j) + dfs(grid, i, j + 1) + dfs(grid, i, j - 1); 
     }
 }
