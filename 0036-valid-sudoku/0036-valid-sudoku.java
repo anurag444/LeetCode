9dbaabbb565e5f17abae41f82f2807b831e7c6b9
Anurag Teckchandani <anuragteckchandani@gmail.com>
2025-03-15 22:38:45 -0400

    Time: 1 ms (100%), Space: 44.2 MB (94.93%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,20 +1,24 @@
 class Solution {
-    public boolean isValidSudoku(char[][] board) {
-        Set set = new HashSet();
+    public boolean isValidSudoku(char[][] b) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (b[i][j] != '.') {
+                    for (int k = i + 1; k < 9; k++) 
+                        if (b[k][j] == b[i][j]) return false;
 
-        for(int i = 0; i < 9; i++){
-            for(int j = 0; j < 9; j++){
-                char number = board[i][j];
+                    for (int k = j + 1; k < 9; k++) 
+                        if (b[i][k] == b[i][j]) return false;
 
-                if(number != '.'){
-                    if(!set.add(number + " in row " + i) || 
-                    !set.add(number + " in col " + j) ||
-                    !set.add(number + " in block " + i/3 + "-" + j/3))
-                    return false;
+                    int r = (i / 3) * 3, c = (j / 3) * 3;
+                    for (int x = r; x < r + 3; x++) {
+                        for (int y = c; y < c + 3; y++) {
+                            if ((x != i || y != j) && b[x][y] == b[i][j]) 
+                                return false;
+                        }
+                    }
                 }
             }
         }
-
         return true;
     }
 }
