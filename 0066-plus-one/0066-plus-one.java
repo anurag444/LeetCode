82a5e7d5ef950b21b50eb1e7b037c39de81739ba
Anurag Teckchandani <anuragteckchandani@gmail.com>
2025-03-15 22:28:10 -0400

    Time: 0 ms (100%), Space: 42.1 MB (17.3%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,31 +1,15 @@
-class Solution {
+public class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length;
-        digits[n - 1] = digits[n - 1] + 1;
-        int carry = 0;
-        
-        for(int i = n - 1; i >= 0; i--){
-            digits[i] = digits[i] + carry;
-            if(digits[i] > 9){
-                carry = digits[i] / 10;
-                digits[i] = digits[i] % 10;
-            }else{
-                carry = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
             }
+            digits[i] = 0;
         }
-        
-        if(carry != 0){
-            int[] res = new int[n + 1];
-            res[0] = carry;
-            for(int i = 0; i < n; i++){
-                res[i + 1] = digits[i];
-                
-                return res;
-            }
-        }
-        
-        
-        
-        return digits;
+        int[] result = new int[n + 1];
+        result[0] = 1;
+        return result;
     }
 }
