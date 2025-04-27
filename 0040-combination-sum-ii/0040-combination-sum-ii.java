bd7346edf3b808eb237670f290c1dc158365fe88
Anurag Teckchandani <anuragteckchandani@gmail.com>
2025-04-27 08:32:37 -0400

    Time: 5 ms (19.21%), Space: 43.1 MB (86.4%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(candidates);
+
+        backtrack(res, new ArrayList<>(), candidates, 0, 0, target);
+
+        return res;
+    }
+
+    public void backtrack(List<List<Integer>> res, List<Integer> list, int[] nums, int idx, int sum, int target){
+        //add the temp to res, and we have to add by creating new list due to how java handles references
+        // by using new java will create a copy, if we not use it then everytime list is updated, it will
+        // be updated in the res as well.
+        if(sum == target){
+            res.add(new ArrayList<>(list));
+        }
+        if(sum > target) return;
+
+        for(int i = idx; i < nums.length; i++){
+            if(i > idx && nums[i] == nums[i - 1]) continue;
+            list.add(nums[i]);
+
+            backtrack(res, list, nums, i + 1, sum + nums[i], target);
+
+            list.remove(list.size() - 1);
+        } 
+    }
+}
