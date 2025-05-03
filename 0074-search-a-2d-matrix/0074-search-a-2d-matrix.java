48132da3d2ae001b4ece24636403b1893c6b9c4e
Anurag Teckchandani <anuragteckchandani@gmail.com>
2025-05-03 17:18:29 -0400

    Time: 0 ms (100%), Space: 42 MB (91.91%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,36 +1,44 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
+        //2 steps - first find the row and then binary search in column
+
         int m = matrix.length;
         int n = matrix[0].length;
 
-        int low = 0;
-        int high = m - 1;
+        int start = 0;
+        int end = m - 1;
+
         int mid = 0;
-        while(low <= high){
-            mid = (low + high)/2;
+        
+        while(start <= end){
+            mid = (start + end)/2;
 
-            if(matrix[mid][0] <= target && target <= matrix[mid][n - 1]){
-                break;
+            if(matrix[mid][0] <= target && matrix[mid][n - 1] >= target){
+                //we found the row now do binary search here
+
+                int startrow = 0;
+                int endrow = n - 1;
+
+                int midrow = 0;
+
+                while(startrow <= endrow){
+                    midrow = (startrow + endrow)/2;
+                    if(matrix[mid][midrow] == target) return true;
+
+                    else if(matrix[mid][midrow] > target) endrow = midrow - 1;
+                    else startrow = midrow + 1;
+                }
+
+                return false;
+
             }
-
-            if(matrix[mid][0] > target){
-                high = mid - 1;
-            }else{
-                low = mid + 1;
+            else if(target < matrix[mid][0]){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
             }
         }
-
-        low = 0;
-        high = n - 1;
-        int mid2;
-
-        while(low <= high){
-            mid2 = (low + high)/2;
-
-            if(matrix[mid][mid2] == target) return true;
-            else if(matrix[mid][mid2] < target) low = mid2 +1;
-            else high = mid2 - 1;
-        } 
 
         return false;
     }
