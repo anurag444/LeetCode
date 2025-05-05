264abc94513ae2c0fb98bdcba54979adfc936f79
Anurag Teckchandani <anuragteckchandani@gmail.com>
2025-05-05 12:10:38 -0400

    Time: 32 ms (44.76%), Space: 51.3 MB (84.05%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,29 +1,39 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        int n = nums.length;
         List<List<Integer>> res = new ArrayList<>();
-        Arrays.sort(nums);
-        
-        for(int i = 0; i < n - 1; i++){
-            int start =  i + 1;
-            int end = n - 1;
+        int n = nums.length;
 
-            while(start < end){
-                int sum = nums[start] + nums[end] + nums[i];
-                if(sum == 0){
-                    List<Integer> temp= new ArrayList<>();
-                    temp.add(nums[i]);
-                    temp.add(nums[start]);
-                    temp.add(nums[end]);
-                    res.add(temp);
-                    start++;
-                    end--;
-                } else if(sum < 0) start++;
-                else end--;
+        Arrays.sort(nums);
+
+        for(int i = 0; i < n; i++){
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            int target = -nums[i];
+
+            //from here we do two sum on sorted array
+
+            int low = i + 1;
+            int high = n - 1;
+
+
+            while(low < high){
+                int sum = nums[low] + nums[high];
+
+                if(sum == target){
+                    //we found the values
+                    res.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                    while(low < high && nums[low] == nums[low + 1]) low++;
+                    while(low < high && nums[high] == nums[high - 1])high--;
+                    high--;
+                    low++;
+                }
+                else if(sum > target){
+                    high--;
+                }
+                else{
+                    low++;
+                }
             }
         }
-
-        res = new ArrayList<>(new LinkedHashSet<>(res));
 
         return res;
     }
