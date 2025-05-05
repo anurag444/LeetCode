35c1b5a0047c2dec2c9fcee043abff4cff20f729
Anurag Teckchandani <anuragteckchandani@gmail.com>
2025-05-05 11:48:27 -0400

    Time: 37 ms (7.79%), Space: 49.2 MB (11.69%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,36 +1,45 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> res = new ArrayList<>();
-        HashMap<String, List<String>> map = new HashMap<>();
+        Map<String, List<String>> map = new HashMap<>();
 
         for(String str : strs){
-            createHash(map, str);
+            checkPattern(map, str);
         }
 
-        for(Map.Entry<String,List<String>> entry : map.entrySet()){
+        //now we have the hashmap
+
+        for(Map.Entry<String, List<String>> entry : map.entrySet()){
             res.add(entry.getValue());
         }
 
         return res;
+        
     }
 
-    public void createHash(HashMap<String, List<String>> map, String str){
-        int[] freq = new int[26];
+    public void checkPattern(Map<String, List<String>> map, String str){
+        //first we generate the pattern string
 
+        String pattern = "";
+
+        int[] freq = new int[26];
         for(char c : str.toCharArray()){
             freq[c - 'a']++;
         }
-        StringBuilder keyBuilder = new StringBuilder();
-            for (int count : freq) {
-                keyBuilder.append("#").append(count);
-            }
-            String hashValue = keyBuilder.toString();
 
-        if(!map.containsKey(hashValue)){
-            map.put(hashValue, new ArrayList<>());
+        for(int i = 0; i < 26; i++){
+            pattern = pattern +  freq[i] + "#";
         }
-        List<String> list = map.get(hashValue);
-        list.add(str);
-        map.put(hashValue, list);
+
+        //we create a new array list if key is not present
+        map.putIfAbsent(pattern, new ArrayList<>());
+
+        List<String> temp = new ArrayList<>();
+        temp = map.get(pattern);
+        temp.add(str);
+
+        map.put(pattern, temp);
     }
+
+
 }
