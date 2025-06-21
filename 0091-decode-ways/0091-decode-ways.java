2ba018f647478ba5ba935e41a40fb942066d7896
Anurag Teckchandani <anuragteckchandani@gmail.com>
2025-06-20 21:54:38 -0400

    Time: 0 ms (100%), Space: 41.7 MB (87.27%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,42 +1,20 @@
 class Solution {
     public int numDecodings(String s) {
         int n = s.length();
+
         int[] dp = new int[n + 1];
+        dp[n] = 1;
 
-        dp[0] = 1;
+        for(int i = n - 1; i >= 0; i--){
+            dp[i] = dp[i + 1];
 
-        for(int i = 1; i <= n; i++){
-            char c = s.charAt(i - 1);
+            if(s.charAt(i) == '0') dp[i] = 0;
 
-            
-                dp[i] = dp[i - 1];
+            if(i < n - 1 && (s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i + 1) < '7'))){
+                dp[i] += dp[i + 2];
+            }
 
-                if(c == '0'){
-
-                    dp[i] = 0;
-                }
-
-                if(i > 1 && (s.charAt(i - 2) == '1' || s.charAt(i - 2) == '2' && s.charAt(i - 1) < '7')){
-                    dp[i] += dp[i - 2];
-                }
-            
         }
-
-        return dp[n];
+        return dp[0];
     }
-
-    // public int ways(int i, String s){
-    //     int n = s.length();
-
-    //     if(i == n) return 1;
-    //     if(s.charAt(i) == '0') return 0;
-
-    //     int res = ways(i +1, s);
-
-    //     if(i < n - 1 &&(s.charAt(i) == '1' || s.charAt(i) == '2' && s.charAt(i + 1) < '7')){
-    //         res += ways(i + 2, s);
-    //     }
-
-    //     return res;
-    // }
 }
