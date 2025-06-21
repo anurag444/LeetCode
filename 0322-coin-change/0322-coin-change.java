dd57d4037ac725050d19762429419b9c55f8ef67
Anurag Teckchandani <anuragteckchandani@gmail.com>
2025-06-20 22:23:34 -0400

    Time: 16 ms (58.42%), Space: 44.5 MB (65.75%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,17 +1,23 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+
         int[] dp = new int[amount + 1];
 
-        int m = coins.length;
+        //this is the base case, means to get amount 0 we need 0 coins
         dp[0] = 0;
-        for(int j = 1; j <= amount; j++){
-           dp[j] = Integer.MAX_VALUE;
+
+        //initialize rest with MAX_VALUE
+
+        for(int i = 1; i <= amount; i++){
+            dp[i] = Integer.MAX_VALUE;
         }
 
-        for(int i = 0; i < m; i++){
-            for(int j = 1; j <= amount; j++){
-                if(coins[i] <= j && dp[j - coins[i]] != Integer.MAX_VALUE){
-                    dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1); 
+        //now iterate over the each of the elements 
+        for(int i = 1; i <= amount; i++){
+            for(int j = 0; j < coins.length; j++){
+                if(coins[j] <= i && dp[i - coins[j]] != Integer.MAX_VALUE){
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
                 }
             }
         }
