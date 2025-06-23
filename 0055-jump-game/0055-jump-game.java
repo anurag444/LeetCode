7e9b4a4e8d9a9c2249c52ea5c4f57997487b601c
Anurag Teckchandani <anuragteckchandani@gmail.com>
2025-06-23 19:01:41 -0400

    Time: 2 ms (83.03%), Space: 46.1 MB (21.97%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,37 +1,16 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int n = nums.length;
-        
-        
-        //Better approach
-        
-        // set final index as end
-        int end = n - 1;
-        
-        for(int i = n-2; i >= 0; i--){
-            // if the value at curr index + the index is greater than end, then set end to current index
-            if(i + nums[i] >= end){
-                end = i;
-            }
+        int maxReach = 0;
+
+        for(int i = 0; i < n; i++){
+            if(i > maxReach) return false;
+            maxReach = Math.max(maxReach, i + nums[i]);
+            if(maxReach >= n - 1) return true;
         }
+
+        return false;
+
         
-        return end == 0;
-        
-//         boolean[] res = new boolean[n];
-//         res[n - 1] = true;
-//         for(int i = n - 2; i >= 0; i--){
-//             int k = nums[i];
-            
-//             while(k > 0){
-//                 if(i + k < n && res[i + k] == true){
-//                     res[i] = true;
-//                     break;
-//                 } 
-//                 k--;
-//             }
-        
-//         }
-        
-//         return res[0];
     }
 }
