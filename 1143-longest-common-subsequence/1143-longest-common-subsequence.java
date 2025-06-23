4960a0548725d69db8688ebf2b952abbc064dca4
Anurag Teckchandani <anuragteckchandani@gmail.com>
2025-06-23 14:49:30 -0400

    Time: 21 ms (53.64%), Space: 51.4 MB (10.16%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -5,18 +5,10 @@ class Solution {
 
         int[][] dp = new int[m + 1][n + 1];
 
-        for(int i = 0; i < n; i++){
-            dp[0][i] = 0;
-        }
-
-        for(int i = 0; i < m; i++){
-            dp[i][0] = 0;
-        }
-
-
-        for(int i = 1; i <= m; i++){
+        for(int i = 1; i <= m;i++){
             for(int j = 1; j <= n; j++){
-                if(text1.charAt(i - 1) == text2.charAt(j - 1)){
+
+                if(text1.charAt(i - 1) == text2.charAt(j- 1)){
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 }
                 else{
@@ -26,5 +18,6 @@ class Solution {
         }
 
         return dp[m][n];
+
     }
 }
