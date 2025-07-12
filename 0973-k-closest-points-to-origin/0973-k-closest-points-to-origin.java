51ed25d9c90535a2c8fbcb2caebd5039efd9af10
Anurag Teckchandani <anuragteckchandani@gmail.com>
2025-07-12 12:37:50 -0400

    Time: 31 ms (56.79%), Space: 51.1 MB (67.02%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -2,38 +2,22 @@ class Solution {
     public int[][] kClosest(int[][] points, int k) {
         int[][] res = new int[k][2];
 
-        int n = points.length;
-        Map<Double, Integer> dist = new HashMap<>();
-        for(int i = 0; i < n; i++){
-            //calc dist and add to dist arr
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+            (a, b) -> Integer.compare(b[0] * b[0] + b[1] * b[1], a[0] * a[0] + a[1] * a[1])
+        );
 
-            double leng = Math.sqrt((points[i][0] * points[i][0]) + (points[i][1] * points[i][1]));
+        for(int[] point: points){
+            pq.offer(point);
 
-            dist.put(leng, i);
+            if(pq.size() > k) pq.poll();
         }
 
-        PriorityQueue<Double> pq = new PriorityQueue<>(Comparator.reverseOrder());
-
-        for(Map.Entry<Double, Integer> entry: dist.entrySet()){
-            pq.offer(entry.getKey());
-
-            if(pq.size() > k){
-                pq.poll();
-            }
-        }
-
-        Iterator<Double> it = pq.iterator();
-        int i = 0;
-
-        while(it.hasNext()){
-            Double d = it.next();
-
-            int idx = dist.get(d);
-
-            res[i] = points[idx];
-            i++;
+        for(int i = 0; i < k; i++){
+            res[i] = pq.poll();
         }
 
         return res;
+
+
     }
 }
